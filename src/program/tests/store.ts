e5f092c97b3ecae214eab0e8f714ef08bb758ec5
Kelechi Oliver Azorji <kelechioliver96@gmail.com>
2022-07-13 13:40:21 +0100

    update tests cases

--- src/program/tests/store.ts
+++ src/program/tests/store.ts
@@ -10,7 +10,7 @@ describe('Store', async () => {
 
     const program = anchor.workspace.Store as Program<Store>;
 
-    it('Sets and changes name!', async () => {
+    it('Create Customer', async () => {
         const [userStatsPDA, _] = await PublicKey.findProgramAddress(
             [anchor.utils.bytes.utf8.encode('user-stats'), provider.wallet.publicKey.toBuffer()],
             program.programId
@@ -27,7 +27,7 @@ describe('Store', async () => {
         expect((await program.account.customers.fetch(userStatsPDA)).name).to.equal('brian');
     });
 
-    it('Add comic', async () => {
+    it('Add comic1', async () => {
         const [userStatsPDA, _] = await PublicKey.findProgramAddress(
             [anchor.utils.bytes.utf8.encode('user-stats'), provider.wallet.publicKey.toBuffer()],
             program.programId
@@ -42,7 +42,24 @@ describe('Store', async () => {
             .rpc();
 
         const userStat = await program.account.customers.fetch(userStatsPDA);
-        console.log(userStat);
         expect(userStat.comics).to.contain('comic1');
+    });
+
+    it('Add comic2', async () => {
+        const [userStatsPDA, _] = await PublicKey.findProgramAddress(
+            [anchor.utils.bytes.utf8.encode('user-stats'), provider.wallet.publicKey.toBuffer()],
+            program.programId
+        );
+
+        await program.methods
+            .addComic('comic2')
+            .accounts({
+                user: provider.wallet.publicKey,
+                customer: userStatsPDA,
+            })
+            .rpc();
+
+        const userStat = await program.account.customers.fetch(userStatsPDA);
+        expect(userStat.comics).to.contain('comic2');
     });
 });
