63105d215dd15e282deebb50f59dbacca982f66d
Kelechi Oliver Azorji <kelechioliver96@gmail.com>
2022-08-04 02:09:58 +0100

    add more fixes

--- src/client/utils/constants.ts
+++ src/client/utils/constants.ts
@@ -1,3 +1,3 @@
 export const NON_BREAKING_SPACE = '\u00a0';
 export const CLUSTER_ENDPOINT = process.env.NEXT_PUBLIC_CLUSTER_ENDPOINT || 'https://api.devnet.solana.com';
-export const SHOP_PROGRAM_ID = process.env.PUBLIC_SHOP_PROGRAM_ID || '';
+export const SHOP_PROGRAM_ID = process.env.NEXT_PUBLIC_SHOP_PROGRAM_ID || '';
