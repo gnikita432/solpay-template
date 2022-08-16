d8fbf53ec7f5a7bd60bedddbf323e274fb875f15
Kelechi Oliver Azorji <kelechioliver96@gmail.com>
2022-08-16 02:45:20 +0100

    replace host config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -65,8 +65,8 @@ const App: FC<CustomAppProps> & { getInitialProps(appContext: AppContext): Promi
 App.getInitialProps = async (appContext) => {
     const props = await NextApp.getInitialProps(appContext);
     const { req } = appContext.ctx;
-    // const host = req?.headers.host || 'localhost:3000';
-    const host = '4203-197-211-58-183.sa.ngrok.io';
+    const host = req?.headers.host || 'localhost:3000';
+    // const host = 'd277-197-211-59-123.eu.ngrok.io';
     return {
         ...props,
         host,
